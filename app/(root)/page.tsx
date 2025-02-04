fe8e3f72fccdc9d8b28919e3ab446109f84781a9
Minh Nguyen <minhng@haposoft.com>
2025-02-04 16:00:20 +0700

    implement search bar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,17 @@
-import Image from "next/image";
 import Header from "../components/Header";
 
-export default function Home() {
+export default async function Home({ searchParams }: {
+  searchParams: Promise<{ query?: string }>
+}) {
+  const query = (await searchParams).query;
+
   return (
     <>
       <Header heading={
         <>
           Pitch your startup, <br />connect with entrepreneurs
         </>
-      } subHeading="Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions" />
-
+      } subHeading="Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions" query={query} />
     </>
   );
 }
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,14 +1,19 @@
+import SearchForm from "./SearchForm";
+
 interface HeaderProps {
     heading: React.ReactNode;
-    subHeading: string;
+    subHeading?: string;
+    query?: string;
 }
 
-export default function Header({ heading, subHeading }: HeaderProps) {
+export default function Header({ heading, subHeading, query }: HeaderProps) {
     return (
         <section className="pink_container">
             <h1 className="heading">{heading}</h1>
 
             <p className="sub-heading !max-w-3xl">{subHeading}</p>
+
+            <SearchForm query={query} />
         </section>
     )
 }
