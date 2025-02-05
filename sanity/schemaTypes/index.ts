4287ff1f6ec66fef21f7d4430cd43c12adff417d
Minh Nguyen <minhng@haposoft.com>
2025-02-05 10:32:44 +0700

    create sanity schema

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,7 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import { startup } from './startup'
+import { author } from './author'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [author, startup],
 }
